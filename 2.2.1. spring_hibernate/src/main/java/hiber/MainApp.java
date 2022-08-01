af912d777dbd96de99b6e211f3525633688f1f12
Kirill9579 <kirill.selivanov95@yandex.ru>
2022-08-01 20:02:54 +0300

    fixed 2

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -16,12 +16,12 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("model1", 101)));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("model2", 102)));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("model3", 103)));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("model4", 104)));
+      userService.addUser(new User("User1", "Lastname1", "user1@mail.ru", new Car("model1", 101)));
+      userService.addUser(new User("User2", "Lastname2", "user2@mail.ru", new Car("model2", 102)));
+      userService.addUser(new User("User3", "Lastname3", "user3@mail.ru", new Car("model3", 103)));
+      userService.addUser(new User("User4", "Lastname4", "user4@mail.ru", new Car("model4", 104)));
 
-      List<User> users = userService.listUsers();
+      List<User> users = userService.getListUsers();
       for (User user : users) {
          System.out.println("Id = " + user.getId());
          System.out.println("First Name = " + user.getFirstName());
